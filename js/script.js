fe9bb93028275d9b1922b19540e6fc29ea1b9868
Alejandro Arzate <fidel_apa_@hotmail.com>
2023-10-19 19:06:49 -0600

    proyecto biblioteca

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Puedes agregar animaciones o interacciones específicas aquí
+
+// Por ejemplo, un ejemplo simple de animación con JavaScript:
+document.querySelector('h1').addEventListener('click', function () {
+    alert('¡Bienvenido a la Biblioteca Alejandría!');
+});
